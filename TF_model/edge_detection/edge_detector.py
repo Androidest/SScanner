327b980f23b3550fd8a8ac4fef6510dc3128d576
keyu chen <775208472@qq.com>
2021-08-30 18:23:44 +0800

    Testing different backend:VGG16,ResNet,MobileNet

--- TF_model/edge_detection/edge_detector.py
+++ TF_model/edge_detection/edge_detector.py
@@ -2,8 +2,6 @@
 import cv2
 import numpy as np
 import tensorflow as tf
-import tensorboard
-import matplotlib.pyplot as plt
 
 
 def loadData(split_rate=0.8):
@@ -15,7 +13,7 @@ def loadData(split_rate=0.8):
         image_string = tf.io.read_file(y_fname)
         image_decoded = tf.image.decode_jpeg(image_string, channels=1)
         y = tf.cast(image_decoded, tf.float32) / 255.0
-        
+
         return x, y
 
     x = tf.data.Dataset.list_files('./dataset/x/*.jpg', shuffle=False)
@@ -26,8 +24,8 @@ def loadData(split_rate=0.8):
 
     ds = tf.data.Dataset.from_tensor_slices((x, y))
     ds = ds.shuffle(buffer_size=len(x), seed=123).map(parse_files)
-    ds = ds.prefetch(buffer_size=8)
-    print('data size: '+ str(len(x)))
+    # ds = ds.prefetch(buffer_size=8)
+    print('data size: ' + str(len(x)))
 
     spliter = int(len(x) * split_rate)
     ds_train = ds.take(spliter)
@@ -36,18 +34,20 @@ def loadData(split_rate=0.8):
 
 
 def create_model():
-    base_model = tf.keras.applications.VGG16(include_top=False, input_shape=(None, None, 3))
+    base_model = tf.keras.applications.mobilenet_v2.MobileNetV2(
+        include_top=False, input_shape=(None, None, 3))
     base_model.trainable = False
     base_model.summary()
 
     input = base_model.input
-    hx = base_model.layers[9].output
-    output = tf.keras.layers.Conv2DTranspose(filters=1, kernel_size=4*2, strides=4, padding='same')(hx)
+    hx = base_model.layers[56].output  # VGG:9, MobilenetV2:26
+    output = tf.keras.layers.Conv2DTranspose(
+        filters=1, kernel_size=8*2, strides=8, padding='same')(hx)
 
     model = tf.keras.Model(inputs=input, outputs=output)
     model.summary()
 
-    opt_fn = tf.keras.optimizers.Adam(0.0001)
+    opt_fn = tf.keras.optimizers.Adam(0.01)
     loss_fn = tf.keras.losses.BinaryCrossentropy(from_logits=True)
 
     model.compile(optimizer=opt_fn, loss=loss_fn)
@@ -60,39 +60,60 @@ def train_model(model, ds_train, ds_test=None, initial_epoch=0, epochs=50, batch
         histogram_freq=0,  # How often to log histogram visualizations
         embeddings_freq=1,  # How often to log embedding visualizations
         update_freq="epoch",
-    ) 
+    )
 
     final_epoch = initial_epoch+epochs
     for e in range(initial_epoch, final_epoch):
         x = ds_train.batch(batchSize)
-        model.fit(x=x, epochs=e+1, initial_epoch=e, 
-                # callbacks=[board_cb], # board_cb,
-                # validation_data=ds_test, 
-                # validation_freq=1,
-                verbose=1)
+        model.fit(x=x, epochs=e+1, initial_epoch=e,
+                #   callbacks=[board_cb], # board_cb,
+                  # validation_data=ds_test,
+                  # validation_freq=1,
+                  verbose=1)
 
         print('start training ...')
 
 
-
-ds_train, _ = loadData(split_rate=0.1)
+ds_train, _ = loadData(split_rate=0.5)
 model = create_model()
 model.trainable = False
-train_model(model, ds_train, initial_epoch=0, epochs=1, batchSize=8)
+train_model(model, ds_train, initial_epoch=0, epochs=4, batchSize=16)
 model.trainable = True
-train_model(model, ds_train, initial_epoch=0, epochs=10, batchSize=8)
+train_model(model, ds_train, initial_epoch=0, epochs=10, batchSize=16)
 
 
 # %%
 # image_string = tf.io.read_file('./dataset/x/3000.jpg')
 image_string = tf.io.read_file('./a.jpg')
 image_decoded = tf.image.decode_jpeg(image_string, channels=3)
-test = tf.image.resize(image_decoded, (1080,1080), preserve_aspect_ratio=True)
+test = tf.image.resize(image_decoded, (1080, 1080), preserve_aspect_ratio=True)
 test = tf.cast(test, tf.float32) / 127.5 - 1
 test = tf.reshape(test, (1)+test.shape)
-img = (model.predict(test)[0,:,:,:] > 0) * 255
+p = model.predict(test)[0, :, :, :]
+# min, max = np.min(p), np.max(p)
+# p = (p-min) / (max-min)
+p = p > 0.5
+img = p  * 255
 cv2.imwrite("./result.jpg", img)
 
 np.max(img)
 
 # %%
+import tensorflow as tf
+filePath = './Models/edge_detector_MobileNetV2.h5'
+model.save(filePath, overwrite=True, include_optimizer=False)
+
+# %%
+import tensorflow as tf
+filePath = './Models/edge_detector_MobileNetV2.h5'
+model = tf.keras.models.load_model(filePath)
+
+
+# %%
+import tensorflow as tf
+base_model = tf.keras.applications.mobilenet_v2.MobileNetV2(
+        include_top=False, input_shape=(512, 512, 3))
+count =0
+for l in base_model.layers:
+    print(count, l.output.shape, l.name)
+    count += 1
