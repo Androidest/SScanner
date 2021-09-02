97299441a39ccc5183ba90b54ec040c68acc9830
Stephen_Leong <775208472@qq.com>
2021-09-02 08:20:53 +0800

    Optz:use UpSampling+Conv instead of Conv

--- TF_model/edge_detection/edge_detector copy.py
+++ TF_model/edge_detection/edge_detector copy.py
@@ -2,7 +2,6 @@
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
-from tensorflow.keras import layers
 from tensorflow.python.ops.gen_array_ops import concat
 
 def showBaseSummary():
@@ -24,7 +23,7 @@ def predict_test(filename, maxSize=1080):
     image = tf.image.decode_jpeg(tf.io.read_file(filename), channels=3)
     image = tf.image.resize(image, (maxSize, maxSize), preserve_aspect_ratio=True)
     image = tf.cast(tf.reshape(image,(1)+image.shape), tf.float32) / 127.5 - 1
-    p = model.predict(image)[0, :, :, 0]
+    p = model.predict(image)[0, :, :, :]
     resultImg = (p > 0.5)  * 255
     plt.figure(figsize = (10,10))
     plt.imshow(resultImg, cmap='gray')
@@ -38,33 +37,29 @@ def compile_model(input, output, lr):
 
 def create_model(lr=0.001):
     base_model = tf.keras.applications.mobilenet_v2.MobileNetV2( 
-        include_top=False, input_shape=(None, None, 3)) #TODO
+        include_top=False, input_shape=(36, 1080, 3)) #TODO
     base_model.trainable = False # works only before compiling
     # showBaseSummary()
 
     input = base_model.input
     hx = base_model.layers[26].output  # VGG:9, MobilenetV2:26,56
-    hx = tf.keras.layers.UpSampling2D(size=4)(hx)
     hx = tf.keras.layers.Conv2D(filters=1, kernel_size=1, padding='same')(hx)
-    hx = tf.keras.layers.Conv2D(filters=1, kernel_size=3, padding='same')(hx)
-    hx = tf.keras.layers.Conv2D(filters=1, kernel_size=3, padding='same')(hx)
+    hx = tf.keras.layers.Conv2DTranspose(filters=1, kernel_size=4*2, strides=4, padding='same')(hx)
     
     output = tf.keras.layers.Conv2D(filters=1, kernel_size=1, padding='same')(hx) # a non functional placeholder output layer
 
     model = compile_model(input, output, lr)
-    # model.summary()
+    model.summary()
     return model
 
-def insert_deconv(model, layer_index, scale, lr=0.001):
-    model.trainable = False # works only before compiling`````
+def insert_deconv(model, layer_index, kernel_size, strides, lr=0.001):
+    model.trainable = False # works only before compiling
     old_fused = model.layers[-1].input
 
     hx = model.layers[layer_index].output  # VGG:9, MobilenetV2:26,56
-    hx = tf.keras.layers.UpSampling2D(size=scale)(hx)
     hx = tf.keras.layers.Conv2D(filters=1, kernel_size=1, padding='same')(hx)
-    hx = tf.keras.layers.Conv2D(filters=1, kernel_size=3, padding='same')(hx)
-    hx = tf.keras.layers.Conv2D(filters=1, kernel_size=3, padding='same')(hx)
-    
+    hx = tf.keras.layers.Conv2DTranspose( filters=1, kernel_size=kernel_size, strides=strides)(hx)(hx)
+
     fused = tf.concat([hx, old_fused], axis=-1)
     output = tf.keras.layers.Conv2D(filters=1, kernel_size=1, padding='same')(fused)
 
@@ -73,26 +68,21 @@ def insert_deconv(model, layer_index, scale, lr=0.001):
     return new_model
 
 #TODO
-# model = create_model(lr=0.001)
-# model = insert_deconv(model, layer_index=8, kernel_size=2*2, strides=2, lr=0.001)
+model = create_model(lr=0.001)
+model = insert_deconv(model, layer_index=8, kernel_size=2*2, strides=2, lr=0.001)
 
 # %%
 import import_ipynb
-from edge_data_generator import loadData, generate
-
-# generate(w=512, h=512, start=0, numb=3000)
+from edge_data_generator import loadData
 
 ds_train, _ = loadData(split_rate=0.2)
 model = create_model(lr=0.001)
-train_model(model, ds_train, initial_epoch=0, epochs=7, batchSize=16)
-model.trainable = True
-train_model(model, ds_train, initial_epoch=0, epochs=10, batchSize=16)
-# model = insert_deconv(model, layer_index=8, kernel_size=2*2, strides=2, lr=0.001)
-# train_model(model, ds_train, initial_epoch=0, epochs=2, batchSize=16)
+train_model(model, ds_train, initial_epoch=0, epochs=2, batchSize=16)
+model = insert_deconv(model, layer_index=8, kernel_size=2*2, strides=2, lr=0.001)
+train_model(model, ds_train, initial_epoch=0, epochs=2, batchSize=16)
 
 filePath = './Models/edge_detector_MobileNetV1.h5'
 model.save(filePath, overwrite=True, include_optimizer=False)
-#%%
 predict_test("./raw_dataset/test.jpg", maxSize=1080)
 
 
